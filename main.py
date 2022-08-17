02105f1e6d333a71a633cb716da8b415a37a8310
waynekoo kali <chinchyekoo@gmail.com>
2022-08-17 03:05:44 -0400

    update with geo like database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from TwitterHandler import *
 from KeysConstant import *
+import geoip2.database
 
 # to import back dataset
 import ast
@@ -27,9 +28,9 @@ def get_ip_information(ips):
 #
 # with open('datasetip.txt', 'w') as f:
 #     f.write(str(ip_ioc))
-
-# =======
-
+#
+# # =======
+#
 # ip_ioc = {}
 #
 # with open('datasetip.txt','r') as f:
@@ -47,16 +48,34 @@ def get_ip_information(ips):
 # ===
 
 
-f = open('./melting-cobalt/results.json.log', 'r')
-data = json.load(f)
-f.close()
-for eachscan in data:
-    ip_info = get_ip_information(eachscan["ip"])
-    eachscan["country_name"] = ip_info["country_name"]
-    eachscan['isp'] = ip_info['isp']
+# f = open('./melting-cobalt/results.json.log', 'r')
+# data = json.load(f)
+# f.close()
+# for eachscan in data:
+#     ip_info = get_ip_information(eachscan["ip"])
+#     eachscan["country_name"] = ip_info["country_name"]
+#     eachscan['isp'] = ip_info['isp']
+#
+#
+# with open("result.json", "w") as f:
+#     json.dump(data, f, indent=4, sort_keys=True)
 
 
-with open("result.json", "w") as f:
-    json.dump(data, f, indent=4, sort_keys=True)
+reader = geoip2.database.Reader('./GeoLite/GeoLite2-Country.mmdb')
+response = reader.country('128.101.101.101')
+print(response.country.iso_code)
+ip_ioc = {}
 
+with open('datasetip.txt','r') as f:
+   ip_ioc = ast.literal_eval(f.read())
 
+for ip in ip_ioc:
+    try:
+        ip_country = reader.country(ip).country.iso_code
+    except:
+        continue
+    print (ip_country)
+    if(ip_country == "SG"):
+        print("write ip to file")
+        with open('./melting-cobalt/ips2.txt', 'a') as f:
+            f.write(ip + "\n")
