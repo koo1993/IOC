5267747b70471ce8251e18b68235495c3e4ffd2e
waynekoo <chinchyekoo@gmail.com>
2022-08-26 15:44:52 +0800

    update automation to keep track of original cobalt strike sg ip

--- main.py
+++ main.py
@@ -19,10 +19,20 @@ def get_ip_information(ips):
 def empty_file_content(filelocation):
     open(filelocation, 'w').close()
 
+def update_cobaltstrike_ip_tracker(list_ip):
+    ip_temp = set()
 
+    try:
+        with open('CobaltStrikeSGIPTracker.txt', 'r') as f:
+            ip_temp = ast.literal_eval(f.read())
+    except:
+        print() # do nth
+
+    ip_temp.update(list_ip)
+    with open('CobaltStrikeSGIPTracker.txt', 'w') as f:
+        f.write(str(ip_temp))
 
 #=== start
-
 # insert twitter bearer tokken
 twitterhandler = TwitterHandler(twitter_bearer_token)
 
@@ -84,6 +94,13 @@ for ip in ip_ioc:
         with open('./melting-cobalt/sgips_maxmind.txt', 'a') as f:
             f.write(ip + "\n")
 
+# add sg ip from previous sg ip tracker to sg_ioc
+try:
+    with open('CobaltStrikeSGIPTracker.txt', 'r') as f:
+        ip_temp = ast.literal_eval(f.read())
+        sg_ioc.update(ip_temp)
+except:
+    print() # do nth
 
 # write sg ip to ips.txt for verification
 for ip in sg_ioc:
@@ -97,24 +114,30 @@ for ip in sg_ioc:
 with Popen(["python", "./melting-cobalt.py", "-i", "ips.txt"], stdout=PIPE, bufsize=1, universal_newlines=True, cwd="./melting-cobalt") as p:
     for line in p.stdout:
         print(line, end='') # process line here
-
 if p.returncode != 0:
     raise CalledProcessError(p.returncode, p.args)
 
 
 print("verificaiton done")
 
+
+# enrich ip with country_name, isp name?
+sg_ioc = set()
 f = open('./melting-cobalt/results.json.log', 'r')
 data = json.load(f)
 f.close()
 
 for eachscan in data:
-    ip_info = get_ip_information(eachscan["ip"])
+    ip = eachscan["ip"]
+    sg_ioc.add(ip)
+    ip_info = get_ip_information(ip)
     eachscan["country_name"] = ip_info["country_name"]
     eachscan['isp'] = ip_info['isp']
 
 
 with open("result.json", "w") as f:
     json.dump(data, f, indent=4, sort_keys=True)
+
+update_cobaltstrike_ip_tracker(sg_ioc)
 
 print("result is in result.json")
